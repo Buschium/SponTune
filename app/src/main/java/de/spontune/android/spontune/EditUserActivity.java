3a338c3e3f9d31b9d428937853c656567fde9efc
Peter Gandenberger <peter.gandenberger@gmail.com>
2018-08-11 17:33:44 +0200

    Added the profilePicture functionality in the UserActivity and EditUserActivity

--- app/src/main/java/de/spontune/android/spontune/EditUserActivity.java
+++ app/src/main/java/de/spontune/android/spontune/EditUserActivity.java
@@ -1,25 +1,48 @@
 package de.spontune.android.spontune;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 import de.spontune.android.spontune.Data.User;
 
 public class EditUserActivity extends AppCompatActivity {
 
     private User user;
+    private ImageButton btnProfilePicture;
+    private final int PICK_IMAGE_REQUEST = 71;
+    private Uri filePath;
+    //Firebase
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +53,10 @@ public class EditUserActivity extends AppCompatActivity {
         final boolean justRegistered = getIntent().getExtras().getBoolean("justRegistered");
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(uid);
+        storageReference = FirebaseStorage.getInstance().getReference();
         final EditText etUsername = findViewById(R.id.et_username);
         final EditText etDescription = findViewById(R.id.et_description);
+        btnProfilePicture = findViewById(R.id.ib_profilePicture);
 
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -39,6 +64,7 @@ public class EditUserActivity extends AppCompatActivity {
                 user = dataSnapshot.getValue(User.class);
                 etUsername.setText(user.getUsername());
                 etDescription.setText(user.getUserDescription());
+                loadImage();
             }
 
             @Override
@@ -54,13 +80,110 @@ public class EditUserActivity extends AppCompatActivity {
                 user.setUsername(etUsername.getText().toString());
                 user.setUserDescription(etDescription.getText().toString());
                 databaseReference.setValue(user);
+                uploadImage();
                 if(justRegistered) {
                     startActivity(new Intent(EditUserActivity.this, MapsActivity.class));
                 }else{
-                    startActivity(new Intent(EditUserActivity.this, UserActivity.class));
+                    startActivity(new Intent(EditUserActivity.this, UserActivity.class).putExtra("image", filePath.toString()));
                 }
             }
         });
 
+        //click on the profile picture to change it
+        btnProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage();
+            }
+        });
+    }
+
+    /**
+     * try to load the profilePicture from firebase. The image is saved under 'images/[userId]'
+     */
+    private void loadImage(){
+        try {
+        final File localFile = File.createTempFile("images", "jpg");
+        storageReference.child("images/"+ user.getId()).getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                btnProfilePicture.setImageBitmap(bitmap);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+            }
+        });
+        } catch (IOException e ) {}
+    }
+
+    /**
+     * standard image chooser for the profile picture
+     */
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Profile Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    /**
+     * result for the image chooser
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                btnProfilePicture.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * upload the image to firebase in the folder 'images/[userId]'
+     */
+    private void uploadImage() {
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("images/"+ user.getId());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(EditUserActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(EditUserActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
     }
 }
--- app/src/main/java/de/spontune/android/spontune/UserActivity.java
+++ app/src/main/java/de/spontune/android/spontune/UserActivity.java
@@ -1,31 +1,59 @@
 package de.spontune.android.spontune;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 import de.spontune.android.spontune.Data.User;
 
 public class UserActivity extends AppCompatActivity {
 
     private User user;
+    private boolean imageLoaded;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_user);
+
+        // if we changed the image in the @EditUserActivity.java we have to load it via the intent due to lag from the Server
+        String path = getIntent().getStringExtra("image");
+        if(path!=null){
+            Uri filePath = Uri.parse(path);
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                ((ImageView) findViewById(R.id.profile_image)).setImageBitmap(bitmap);
+                imageLoaded = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(uid);
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -36,6 +64,10 @@ public class UserActivity extends AppCompatActivity {
                 tvUsername.setText(user.getUsername());
                 TextView tvDescription = findViewById(R.id.tv_description);
                 tvDescription.setText(user.getUserDescription());
+                // if we have the image form the editUserActivity we don't have to load it
+                if(!imageLoaded) {
+                    loadImage();
+                }
             }
 
             @Override
@@ -51,7 +83,26 @@ public class UserActivity extends AppCompatActivity {
                 startActivity(new Intent(UserActivity.this, EditUserActivity.class).putExtra("justRegistered", false));
             }
         });
+    }
 
+    /**
+     * try to load the profilePicture from firebase. The image is saved under 'images/[userId]'
+     */
+    private void loadImage(){
+        try {
+            final File localFile = File.createTempFile("images", "jpg");
+            FirebaseStorage.getInstance().getReference().child("images/"+ user.getId()).getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                    ((ImageView) findViewById(R.id.profile_image)).setImageBitmap(bitmap);
 
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                }
+            });
+        } catch (IOException e ) {}
     }
 }
